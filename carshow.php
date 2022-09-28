a2ffc46e0b195bcc69716c73cdd6c2d26ca1a43d
premtodev08710 <64456171+premtodev08710@users.noreply.github.com>
2022-09-29 01:33:33 +0700

    30

--- carshow.php
+++ carshow.php
@@ -3,55 +3,55 @@
     <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h6 class="text-primary text-uppercase">// Our Technicians //</h6>
-                <h1 class="mb-5">Our Expert Technicians</h1>
+                <h6 class="text-primary text-uppercase">// รุ่นต่างๆ //</h6>
+                <h1 class="mb-5">รายละเอียดรถ</h1>
             </div>
             <div class="row g-4">
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="team-item">
                         <div class="position-relative overflow-hidden">
-                            <img class="img-fluid" src="img/team-1.jpg" alt="">
-                            <div class="team-overlay position-absolute start-0 top-0 w-100 h-100">
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
+                            <img class="img-fluid" src="img/carousel-1.png" alt="">
+                            
                         </div>
                         <div class="bg-light text-center p-4">
-                            <h5 class="fw-bold mb-0">Full Name</h5>
-                            <small>Designation</small>
+                            <h5 class="fw-bold mb-0">รถรุ่น 000</h5>
+                           <a href=""> <small>รายละเอียด</small></a>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                     <div class="team-item">
                         <div class="position-relative overflow-hidden">
-                            <img class="img-fluid" src="img/team-2.jpg" alt="">
-                            <div class="team-overlay position-absolute start-0 top-0 w-100 h-100">
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
+                            <img class="img-fluid" src="img/carousel-2.png" alt="">
+                            
                         </div>
                         <div class="bg-light text-center p-4">
-                            <h5 class="fw-Team mb-0">Full Name</h5>
-                            <small>Designation</small>
+                            <h5 class="fw-Team mb-0">รถรุ่น 0000</h5>
+                           <a href=""> <small>รายละเอียด</small></a>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                     <div class="team-item">
                         <div class="position-relative overflow-hidden">
-                            <img class="img-fluid" src="img/team-3.jpg" alt="">
-                            <div class="team-overlay position-absolute start-0 top-0 w-100 h-100">
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
+                            <img class="img-fluid" src="img/carousel-1.png" alt="">
+                            
                         </div>
                         <div class="bg-light text-center p-4">
-                            <h5 class="fw-bold mb-0">Full Name</h5>
-                            <small>Designation</small>
+                            <h5 class="fw-bold mb-0">รถรุ่น 0000</h5>
+                           <a href=""> <small>รายละเอียด</small></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="team-item">
+                        <div class="position-relative overflow-hidden">
+                            <img class="img-fluid" src="img/carousel-2.png" alt="">
+                            
+                        </div>
+                        <div class="bg-light text-center p-4">
+                            <h5 class="fw-Team mb-0">รถรุ่น 0000</h5>
+                           <a href=""> <small>รายละเอียด</small></a>
                         </div>
                     </div>
                 </div>
